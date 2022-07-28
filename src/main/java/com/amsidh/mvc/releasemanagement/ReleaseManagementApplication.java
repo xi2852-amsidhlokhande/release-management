69bafce8f2b236a14335fa4d32dfc02c26205f44
Amsidh.Lokhande <amsidh.lokhande@xebia.com>
2022-07-28 13:54:41 +0530

    Initial commit

---
+++ src/main/java/com/amsidh/mvc/releasemanagement/ReleaseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.amsidh.mvc.releasemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amsidh/mvc/releasemanagement/ReleaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsidh.mvc.releasemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
